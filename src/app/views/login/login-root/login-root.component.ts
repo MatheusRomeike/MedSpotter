8c51c98b2809eb858969404864e9b399cd637783
Matheus <matheus_romeike@sisand.com.br>
2023-08-14 20:12:32 -0300

    adjusting responsivity and menus

--- src/app/views/login/login-root/login-root.component.ts
+++ src/app/views/login/login-root/login-root.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-root',
   templateUrl: './login-root.component.html',
-  styleUrls: ['./login-root.component.scss']
+  styleUrls: ['./login-root.component.scss'],
 })
 export class LoginRootComponent {
+  constructor(private router: Router) {}
 
+  login() {
+    this.router.navigate(['/']);
+  }
 }
